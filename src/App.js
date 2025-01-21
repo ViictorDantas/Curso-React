6fa8ef6b63b92f5580f3cb43c656970744ea036f
ViictorDantas <viictordantaz@gmail.com>
2025-01-21 16:09:40 -0300

    refatora o componente App para usar arrow functions e adiciona contador ao estado

--- src/App.js
+++ src/App.js
@@ -5,27 +5,35 @@ import { Component } from 'react';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "Hello, World!"
+    // Vinculando o método handlePClick ao contexto da classe (Foi substituído por arrow function)
+    // this.handlePClick = this.handlePClick.bind(this);
+    
+    state = {
+      message: "Hello, World!",
+      counter: 0
     };
+  
+
+  handlePClick = () => {
+    this.setState({ message: 'olá, mundo!' });
   }
 
-  handlePClick() {
-    const { message } = this.state;
-    console.log(`Clicou no P ${ message }`);
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
   }
 
   render() {
     // modo de usar com destructuring
-    const { message } = this.state;
+    const { message, counter } = this.state;
 
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>My first React App</h1>
-        <p onClick={this.handlePClick}> { message }</p>
+        <p onClick={this.handlePClick}> { message } {counter}</p>
+        <a onClick={this.handleAClick} className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
       </div>
     );
   }
